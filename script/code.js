e780887d71a3a7427297239bbb5704a6b5172175
Alonso0213 <alonsocupido51@gmail.com>
2023-05-31 11:07:35 +0200

    q1 object array

---
+++ script/code.js
@@ -0,0 +1,48 @@
+const people=[
+    {
+        firstName: 'Alonso',
+        lastname: 'cupido',
+        age:'19'
+    },
+    {
+        firstName: 'Amy',
+        lastname: 'Pieters',
+        age:'23'
+    },
+    {
+        firstName: 'Siyamthanda',
+        lastname: 'Mazokwana',
+        age:'24'
+    },
+    {
+        firstName: 'Jason',
+        lastname: 'Barron',
+        age:'23'
+    },
+    {
+        firstName: 'Jason',
+        lastname: 'Viljoen',
+        age:'24'
+    },
+    {
+        firstName: 'Keeno',
+        lastname: 'Fourie',
+        age:'18'
+    },
+    {
+        firstName: 'Logan',
+        lastname: 'Thomas',
+        age:'23'
+    },
+    {
+        firstName: 'Mish-al',
+        lastname: 'Taliep',
+        age:'21'
+    },
+    {
+        firstName: 'Thotyelwa',
+        lastname: 'Mpongwana',
+        age:'23'
+    },
+]
+console.log(people);
